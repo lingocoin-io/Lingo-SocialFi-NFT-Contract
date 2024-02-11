0a6a3548b546aaaf1c96874fdbb73cc04d1ba5a3
Adrien <adrien@ADRIENs-MacBook-Pro.local>
2024-02-11 17:39:21 +0100

    update contract

--- contracts/LingoNFT.sol
+++ contracts/LingoNFT.sol
@@ -7,20 +7,13 @@ import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 
-
-
-//Economy Class URI : ipfs://
-//Business Class URI : ipfs://
-//First Class URI : ipfs://QmTn6hxErt5x95Qn1os9X6eFm9jCpUBACM2u5BZtJ6fwvk
-//PrivateJet URI : ipfs://
-
 contract LingoNFT is ERC721, Ownable, ReentrancyGuard {
     // Minting window
     uint256 public saleStartTime = 0;
     address public economySigner;
     address public businessSigner;
     address public firstSigner;
-    uint256 public firstClassMintPrice; // Set a price for First Class NFTs
+    uint256 public firstClassMintPrice;
     uint256 public maxFirstClassSupply;
     uint256 private firstClassSupplyCounter;
 
@@ -38,7 +31,7 @@ contract LingoNFT is ERC721, Ownable, ReentrancyGuard {
         FIRST_CLASS,
         PRIVATE_JET
     }
-    // Add a new mapping to track whether a user has minted for their assigned tier
+    // Add a new mapping to track whether a user has minted for a specific tier
     mapping(address => mapping(Tier => bool)) private _hasMinted;
 
    modifier isActive() {
@@ -68,15 +61,19 @@ contract LingoNFT is ERC721, Ownable, ReentrancyGuard {
     function setBusinessSigner(address _businessSigner) external onlyOwner {
         businessSigner = _businessSigner;
     }
+    //Function to set First Class signer
     function setFirstSigner(address _firstSigner) external onlyOwner {
         firstSigner = _firstSigner;
     }
+    //Function to set URI for a specific tier
     function setTierURI(Tier tier, string calldata uri) external onlyOwner {
     _tierURIs[tier] = uri;
    }
+   //Function to get URI for a specific tier
    function getTierURI(Tier tier) public view returns (string memory) {
     return _tierURIs[tier];
     }
+    //Function to get Signer for a specific tier
     function getSignerByTier(Tier tier) internal view returns(address)  {
         if(tier == Tier.ECONOMY_CLASS){
             return economySigner;
@@ -91,51 +88,41 @@ contract LingoNFT is ERC721, Ownable, ReentrancyGuard {
     }
     // Function to mint NFTs for Economy, and Business (require being allowlisted) (can only be called during the minting window)
     function mintEconomyBusinessClassNFT(Tier tier, bytes32 r, bytes32 s, uint8 v) external nonReentrant isActive {
-
         require(tier == Tier.ECONOMY_CLASS || tier == Tier.BUSINESS_CLASS, "Incorrect Tier");
         require( !_hasMinted[msg.sender][tier], "Address already minted this tier");
         require( getSignerByTier(tier) != address(0), "Invalid Signer");
-
         bytes32 messageHash = keccak256(abi.encodePacked(msg.sender, tier));
         bytes32 prefixedMessageHash = keccak256(abi.encodePacked("\x19Ethereum Signed Message:\n32", messageHash));
-
         require( ecrecover(prefixedMessageHash, v, r, s) == getSignerByTier(tier), "Unauthorized Signer");
-        // You might want to still check if the signer is correct, but for demonstration,
-        // we're emitting the event regardless of the check's outcome.
+        
         uint256 tokenId = getNextTokenId();
         // Mint NFT to the sender
         _mint(msg.sender, tokenId, _tierURIs[tier], tier);
     }
 
    function mintFirstClassNFT(Tier tier, bytes32 r, bytes32 s, uint8 v) external payable nonReentrant isActive {
-    
         require(tier == Tier.FIRST_CLASS, "This function is only for FIRST_CLASS tier");
         require(msg.value >= firstClassMintPrice, "Ether sent is not correct");
         require(!_hasMinted[msg.sender][Tier.FIRST_CLASS], "Address already minted First");
         require( getSignerByTier(tier) != address(0), "Invalid Signer");
-
         bytes32 messageHash = keccak256(abi.encodePacked(msg.sender, tier));
         bytes32 prefixedMessageHash = keccak256(abi.encodePacked("\x19Ethereum Signed Message:\n32", messageHash));
-
         require( ecrecover(prefixedMessageHash, v, r, s) == getSignerByTier(tier), "Unauthorized Signer");
         
         uint256 tokenId = getNextTokenId();
         _mint(msg.sender, tokenId, _tierURIs[tier], tier);
     }
-
+    //Function to mint and airdrop the Private Jet NFT at the end of the campaign
     function mintPrivateJetNFT(address[] calldata recipients) external nonReentrant onlyOwner {
         uint256 tokenId;
-        
-        // Airdrop NFTs to the specified addresses
         for (uint256 i = 0; i < recipients.length; i++) {
             tokenId = getNextTokenId();
             _mint(recipients[i], tokenId, _tierURIs[Tier.PRIVATE_JET], Tier.PRIVATE_JET);
         }
     }
-   // Fonction améliorée pour airdrop des NFTs à une liste de destinataires pour un tier spécifié
+   // Fonction to allow airdrop of a specific NFT TIer to specific addresses
     function airdropNFT(address[] calldata recipients, Tier tier) external nonReentrant onlyOwner {
         require(tier != Tier.PRIVATE_JET, "Incorrect Tier");
-
         for (uint256 i = 0; i < recipients.length; i++) {
             uint256 tokenId = getNextTokenId();
             _mint(recipients[i], tokenId, _tierURIs[tier], tier);
@@ -146,16 +133,16 @@ contract LingoNFT is ERC721, Ownable, ReentrancyGuard {
         uint256 tokenId = _tokenIdCounter;
         return tokenId;
     }
+    //Function to get the total numer of First Class NFT minted
     function getFirstClassSupply() public view returns (uint256) {
         return firstClassSupplyCounter;
     }
-    // Function to set the start minting date
+    // Function to set the sale start date
     function setSaleStartTime(uint256 _startDate) external onlyOwner {
         //require(_startDate > block.timestamp,"Invalid Start Date");
         saleStartTime = _startDate;
     }
- 
-    // Function to get the start minting date
+    // Function to get the sale start date
     function getSaleStartTime() external view returns (uint256) {
         return saleStartTime;
     }
@@ -163,6 +150,7 @@ contract LingoNFT is ERC721, Ownable, ReentrancyGuard {
     function getTotalSupply() external view returns (uint256) {
         return _tokenIdCounter;
     }
+    //FUnction to get the URI of a specific NFT
     function tokenURI(uint256 tokenId) public view override returns (string memory) {
         require(_exists(tokenId), "Token does not exist");
         // Combine the base URI and token-specific URI to get the full metadata URI
@@ -172,7 +160,6 @@ contract LingoNFT is ERC721, Ownable, ReentrancyGuard {
     function _mint(address to, uint256 tokenId, string memory uri, Tier tier) internal {
         require(!_exists(tokenId), "Token ID already exists");
         require(to != address(0), "Cannot mint to the zero address");
-        
         _hasMinted[msg.sender][tier] = true;
         _tokenIdCounter = _tokenIdCounter + 1;
         _tokenURIs[tokenId] = uri;
@@ -185,15 +172,15 @@ contract LingoNFT is ERC721, Ownable, ReentrancyGuard {
     function getCurrentTimestamp() external view returns (uint256) {
         return block.timestamp;
     }
+    // Function to check if the address has already minted a specific NFT Tier
     function hasMinted(address account, Tier tier) external view returns (bool) {
         return _hasMinted[account][tier];
     }
-    // Function to withdraw Ether from the contract
+    // Function to withdraw funds from the contract
     function withdraw() external onlyOwner {
         uint256 balance = address(this).balance;
         require(balance > 0, "No funds left to withdraw");
-
-        // Transfer Ether to the owner's address
+        // Transfer funds to the owner's address
         (bool sent, ) = owner().call{value: balance}("");
         require(sent, "Transfer failed.");  
     }
